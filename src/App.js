18909448dd696304ff4b2cc50aab02eb6eacd49d
rishabh-318 <rishu2nd@gmail.com>
2024-11-08 11:39:32 +0530

    changed the friend split logic

--- src/App.js
+++ src/App.js
@@ -109,12 +109,12 @@ function Friend({ friend, onSelection, selectedFriend }) {
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
 
-      {friend.balance < 0 && (
+      {friend.balance > 0 && (
         <p className="red">
           {friend.name} ows you {Math.abs(friend.balance)}€
         </p>
       )}
-      {friend.balance > 0 && (
+      {friend.balance < 0 && (
         <p className="green">
           You owe {friend.name} {Math.abs(friend.balance)}€
         </p>
